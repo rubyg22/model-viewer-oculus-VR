2d547aa73a30af17def4aa2e384f4c960096388f
Lize <eliza.knipscher@gmail.com>
2021-07-30 13:47:10 -0700

    Initial commit.

---
+++ Hydrocephalic Ven VR 730/Assets/FollowAvatar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowAvatar : MonoBehaviour
+{
+    [SerializeField] Transform Phrase;
+   // public GameObject ;
+    
+    void Update()
+    {
+        transform.LookAt(Phrase);
+  
+    }
+}
---
+++ Hydrocephalic Ven VR 730/Assets/RotateSegment.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSegment : MonoBehaviour
+{
+    public float rotationSpeed = 50;
+
+    void Update()
+    {
+        float rotationPerFrame = rotationSpeed * Time.deltaTime;
+        {
+            transform.Rotate(0, 0, rotationPerFrame);
+        }
+    }
+}
